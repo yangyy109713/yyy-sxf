9576ec516bd7474e8840295b4bdcd1b918a05098
yuanyuan yang <yangyuanyuan@dianwoda.com>
2019-08-18 11:06:07 +0800

    commit init project

---
+++ src/main/java/com/yyy/rutu/sxfy/SxfyApplication.java
@@ -0,0 +1,13 @@
+package com.yyy.rutu.sxfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SxfyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SxfyApplication.class, args);
+    }
+
+}
